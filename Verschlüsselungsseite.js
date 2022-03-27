8928dc4294ecdad76cbe1524bd03a720098e8ba4
Konstantin <merkelkonstantin@web.de>
2022-03-27 14:04:21 +0200

    finished mal-funktion

--- Verschlüsselungsseite.js
+++ Verschlüsselungsseite.js
@@ -64,22 +64,14 @@ let größer = (one, two) => { //input als String
             return "same"
         }
     }
-
 }
-
-
-
 let plus = (sumone, sumtwo) => { // input als String
     let sumoneArr = Array.from(sumone);
     let sumtwoArr = Array.from(sumtwo);
-    if (sumone == "0") {
-
-    } else {
+    if (sumone == "0") {} else {
         sumoneArr = deleteprenulls(sumoneArr);
     }
-    if (sumtwo == "0") {
-
-    } else {
+    if (sumtwo == "0") {} else {
         sumtwoArr = deleteprenulls(sumtwoArr);
     }
     let übertragung = 0;
@@ -101,11 +93,14 @@ let plus = (sumone, sumtwo) => { // input als String
         sumtwoArr.unshift("0")
     } // die Zahl mit ursprünglich mehr Ziffer bekommt eine Null hinzugefügt, die andere so viele dass sie so viele hat wie die andere zuvor
     for (let i = 0; i < runthrow; i++) {
-        if (parseInt(sumoneArr[sumoneArr.length - 1 - i]) + parseInt(sumtwoArr[sumtwoArr.length - 1 - i]) + übertragung < 10) {
-            resultinverted += String(parseInt(sumoneArr[sumoneArr.length - 1 - i]) + parseInt(sumtwoArr[sumtwoArr.length - 1 - i]) + übertragung)
+        if (parseInt(sumoneArr[sumoneArr.length - 1 - i]) +
+            parseInt(sumtwoArr[sumtwoArr.length - 1 - i]) + übertragung < 10) {
+            resultinverted += String(parseInt(sumoneArr[sumoneArr.length - 1 - i]) +
+                parseInt(sumtwoArr[sumtwoArr.length - 1 - i]) + übertragung)
             übertragung = 0;
         } else {
-            resultinverted += String(String(parseInt(sumoneArr[sumoneArr.length - 1 - i]) + parseInt(sumtwoArr[sumtwoArr.length - 1 - i]) + übertragung - 10))
+            resultinverted += String(String(parseInt(sumoneArr[sumoneArr.length - 1 -
+                i]) + parseInt(sumtwoArr[sumtwoArr.length - 1 - i]) + übertragung - 10))
             übertragung = 1;
         }
     } // für alle Ziffern wird von hinten nach vorne die Addition durchgeführt (bei größer 10 wird in übertragung gespeichert)
@@ -121,12 +116,6 @@ let plus = (sumone, sumtwo) => { // input als String
     }
     return outputAsString;
 }
-
-
-
-
-
-
 let minus = (minuend, substrahend) => { //minuend-substrahend; input als String
     übertragung = 0;
     result = "";
@@ -140,14 +129,17 @@ let minus = (minuend, substrahend) => { //minuend-substrahend; input als String
         substrahendArr.unshift("0");
     }
     if (größer(minuend, substrahend) == substrahend) {
-        return "-42" //wenn substarhend > minuend wird -42 zurückgegeben ist zwar nicht richtig aber wurzel schaut nur >0
+        return "-42" //wenn substarhend > minuend wird -42 zurückgegeben ist zwar nicht richtig aber wurzel schaut nur > 0
     } else {
         for (let i = 0; i < minuendArr.length; i++) {
-            if (parseInt(minuendArr[minuendArr.length - 1 - i]) - parseInt(substrahendArr[substrahendArr.length - 1 - i]) - übertragung >= 0) {
-                resultinverted += String(parseInt(minuendArr[minuendArr.length - 1 - i]) - parseInt(substrahendArr[substrahendArr.length - 1 - i]) - übertragung)
+            if (parseInt(minuendArr[minuendArr.length - 1 - i]) -
+                parseInt(substrahendArr[substrahendArr.length - 1 - i]) - übertragung >= 0) {
+                resultinverted += String(parseInt(minuendArr[minuendArr.length - 1 -
+                    i]) - parseInt(substrahendArr[substrahendArr.length - 1 - i]) - übertragung)
                 übertragung = 0;
             } else {
-                resultinverted += String(parseInt(minuendArr[minuendArr.length - 1 - i]) - parseInt(substrahendArr[substrahendArr.length - 1 - i]) - übertragung + 10)
+                resultinverted += String(parseInt(minuendArr[minuendArr.length - 1 -
+                    i]) - parseInt(substrahendArr[substrahendArr.length - 1 - i]) - übertragung + 10)
                 übertragung = 1;
             }
         } //führt die Subtraktion schritt für Schritt von hinten nach vorne aus
@@ -166,31 +158,49 @@ let minus = (minuend, substrahend) => { //minuend-substrahend; input als String
         return givebackresult;
     }
 }
-
 let mal = (faktorOne, faktorTwo) => { // noch nicht fertig
     let faktorOneArr = Array.from(faktorOne);
     let faktorTwoArr = Array.from(faktorTwo);
-    let übertragung = "0";
     faktorOneArr = invertArr(faktorOneArr);
     faktorTwoArr = invertArr(faktorTwoArr);
+    let übertragung = "0";
     let toAdd = [];
     let pushingIntoToAdd = "";
     for (let i = 0; i < faktorOneArr.length; i++) {
-        for (let k = 0; k < faktorTwoArr; k++) {
-            if (größer(String(parseInt(faktorOneArr[i]) * parseInt(faktorOneArr[k])), "10") == "10") {
-                pushingIntoToAdd += String(parseInt(faktorOneArr[i]) * parseInt(faktorTwoArr[k]));
-                console.log(parseInt(faktorOneArr[i]), parseInt(faktorTwoArr[k]))
+        for (let j = 0; j < i; j++) {
+            pushingIntoToAdd += "0"
+        }
+        for (let k = 0; k < faktorTwoArr.length; k++) {
+            if (größer(plus(String(parseInt(faktorOneArr[i]) * parseInt(faktorTwoArr[k])), übertragung), "10") == "10") {
+                pushingIntoToAdd += plus(String(parseInt(faktorOneArr[i]) * parseInt(faktorTwoArr[k])), übertragung);
                 übertragung = "0"
             } else {
-                let ArrForLastDigit = Array.from(String(parseInt(faktorOneArr[i]) * parseInt(faktorOneArr[k])));
+                let ArrForLastDigit = Array.from(plus(String(parseInt(faktorOneArr[i]) * parseInt(faktorTwoArr[k])), übertragung));
                 pushingIntoToAdd += ArrForLastDigit[1];
                 übertragung = ArrForLastDigit[0];
             }
         }
+        pushingIntoToAdd += übertragung;
+        toAdd.push(pushingIntoToAdd);
+        pushingIntoToAdd = "";
+        übertragung = "0"
     }
-}
-mal("20", "2")
+    for (let i = 0; i < toAdd.length; i++) {
+        let invertedNumberArr = invertArr(Array.from(toAdd[i]))
+        let invertedNumber = "";
+        for (let k = 0; k < invertedNumberArr.length; k++) {
+            invertedNumber += invertedNumberArr[k];
+        }
+        toAdd[i] = invertedNumber;
+    }
+    let result = "0";
+    for (let i = 0; i < toAdd.length; i++) {
+        result = plus(result, String(toAdd[i]))
+    }
+    return result;
 
+}
+console.log(mal("96", "1"))
 let wurzelziehen = (underwurzel) => {
     let wurzelArr = Array.from(String(underwurzel));
     wurzelArr = deleteprenulls(wurzelArr);
@@ -230,13 +240,10 @@ let wurzelziehen = (underwurzel) => {
         let Arrzr = [];
         if (minus(numberforprocessStr, calcstartStr) >= 0) {
             for (let k = 0; größer(numberforprocessStr, "0") != 0; k++) {
-                numberforprocessStr = minus(numberforprocessStr, plus(String(k * 2), calcstartStr));
+                numberforprocessStr = minus(numberforprocessStr, plus(String(k *
+                    2), calcstartStr));
                 Arrzr.push(numberforprocessStr);
-                console.log(numberforprocessStr)
-                console.log(plus(String(k * 2), calcstartStr))
-                if (größer(numberforprocessStr, "0") == "0") {
-
-                } else {
+                if (größer(numberforprocessStr, "0") == "0") {} else {
                     rest = numberforprocessStr;
                 }
             }
@@ -252,30 +259,18 @@ let wurzelziehen = (underwurzel) => {
     }
     return result;
 }
-
-
-
-
-
-
-
-
-
 let paraone;
 let paratwo;
-
 let ersteWurzel = () => {
     let wurzel = document.getElementById("wurzel1").value;
     paraone = wurzelziehen(wurzel);
     console.log(paraone);
 }
-
 let zweiteWurzel = () => {
     let wurzel = document.getElementById("wurzel2").value;
     paratwo = wurzelziehen(wurzel);
     console.log(paratwo);
 }
-
 if (paraone && paratwo != 0) {
     console.log("runs")
 }
